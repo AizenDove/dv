b5f2a3e46709b997befd2c48ff506e649cdf83c6
Ragnar Rally <145118951+RagRolf@users.noreply.github.com>
2025-03-11 15:38:34 +0100

    Senaste

--- VR Template/Assets/LEDController.cs
+++ VR Template/Assets/LEDController.cs
@@ -1,16 +1,40 @@
+using System;
 using System.IO.Ports;
+using UnityEditor;
 using UnityEngine;
 
 public class LEDController : MonoBehaviour
 {
-    SerialPort serialPort = new SerialPort("COM3", 11000); // Match with your Arduino's COM port
-
+    [SerializeField] string portName;
+    SerialPort serialPort = new SerialPort(null, 11000); // Match with your Arduino's COM port
     void Start()
     {
+        if (portName != null)
+        {
+            serialPort.PortName = portName;
+        }
+#if UNITY_EDITOR
+        else
+        {
+            Debug.LogError("Lägg till namn på porten...");
+            EditorApplication.isPlaying = false;
+        }
+#endif
         if (!serialPort.IsOpen)
         {
-            serialPort.Open();
-            serialPort.ReadTimeout = 100;
+            string[] ports = SerialPort.GetPortNames();
+            if (Array.Exists(ports, p => p == "COM4"))
+            {
+                serialPort.Open();
+                serialPort.ReadTimeout = 100;
+            }
+#if UNITY_EDITOR
+            else
+            {
+                Debug.LogError("Oops, PORT 4 är stängd. Vi avslutar...");
+                EditorApplication.isPlaying = false;
+            }
+#endif
         }
     }
 
